9ec6a0c9e400c8857fd8f53b3dcff8ff20034966
msimonin <matthieu.simonin@inria.fr>
2013-10-04 22:43:44 +0200

    fix version
    M

--- distributions/deb-package/build.sh
+++ distributions/deb-package/build.sh
@@ -20,7 +20,7 @@
 #
 
 SNOOZE_PACKAGE_NAME="snoozenode"
-SNOOZE_JAR_NAME="uber-snoozenode-2.0.1-SNAPSHOT.jar"
+SNOOZE_JAR_NAME="uber-snoozenode-2.1.0-SNAPSHOT.jar"
 
 # Update config files
 cp -R ../../configs/framework/quartz.properties $SNOOZE_PACKAGE_NAME/debian/input/configs/
